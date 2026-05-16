6f2257fc33aafa94ddd672b0006baebe764eeff5
ac_orz <94980195+hank1101444@users.noreply.github.com>
2026-05-16 16:08:16 +0000

    fix: restore health check response

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
 
   app.get('/health', async () => {
     return {
-      status: 'fail' // fail
+      status: 'ok' // fail
     };
   });
 
